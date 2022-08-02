002977645069e8ceeae35afb49a8dc7b95a8227a
Ali Dölen <40203322+AliDolen@users.noreply.github.com>
2022-08-02 15:49:44 +0300

    Add files via upload

---
+++ Netlix-CloneApp/Netlix-CloneApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netlix-CloneApp
+//
+//  Created by ali dölen on 2.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
